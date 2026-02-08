fa15ec10b46b753da5bfd70c00b62c42a99e621c
Ankit Jaiswal <codewithankitjaiswal@gmail.com>
2026-02-08 09:39:29 +0530

    added db connection link and code

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+import connectDB from "./db/index.js";
+
+connectDB()
+// .then(() => {
+//     // app.listen(process.env.PORT || 8000, () => {
+//     //     console.log(`Server is running at port: ${process.env.PORT}`);
+//     // })
+// })
+// .catch((err) => {
+//     console.log("MONGO DB connection failed !!! ", err);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("DB not able to intract with express, Error: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("ERROR: ", error);
+        throw err
+    }
+})()
+*/
